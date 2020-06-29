cf702d9740c186aff071717e140c666954c39540
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-29 18:24:22 -0300

    C28A423 - Adding Input Validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,36 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 const { create } = require('../models/user');
 
 module.exports = {
     createUser: async function(args, req) {
+        const errors = [];
+        
+        if (validator.isEmpty(args.userInput.name)) {
+            errors.push({ message: 'Name is required'});
+        }
+        if (!validator.isLength(args.userInput.name, { min: 5 })) {
+            errors.push({ message: 'Name must be at least 5 characters long'});
+        }
+        if (validator.isEmpty(args.userInput.email)) {
+            errors.push({ message: 'E-mail is required'});
+        }
+        if (!validator.isEmail(args.userInput.email)) {
+            errors.push({ message: 'Please enter a valid e-mail address'});
+        }
+        if (validator.isEmpty(args.userInput.password)) {
+            errors.push({ message: 'Password is required'});
+        }
+        if (!validator.isLength(args.userInput.password, { min: 5 })) {
+            errors.push({ message: 'Password must be at least 5 characters long'});
+        }
+        
+        if (errors.length > 0) {
+            throw new Error('Invalid input');
+        }
+        
         const email = args.userInput.email;
         const name = args.userInput.name;
         const password = args.userInput.password;
