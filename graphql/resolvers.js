e248caf17a7404ddec410717bf162f05162a8dd5
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-30 17:55:10 -0300

    C28A438 - Updating Posts

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -204,12 +204,76 @@ module.exports = {
             error.code = 404;
             throw error;
         }
-        
+
         return {
             ...post._doc,
             _id: post._id.toString(),
             createdAt: post.createdAt.toISOString(),
             updatedAt: post.updatedAt.toISOString()
+        }
+    }
+
+
+    ,
+
+
+    updatePost: async function({ id, postInput}, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated');
+            error.code = 401;
+            throw error;
+        }
+
+        const post = await Post.findById(id).populate('creator');
+
+        if (!post) {
+            const error = new Error('Post not found');
+            error.code = 404;
+            throw error;
+        }
+
+        if (post.creator._id.toString() !== req.userId.toString()) {
+            const error = new Error('Not authorized');
+            error.code = 403;
+            throw error;
+        }
+
+        const errors = [];
+
+        if (validator.isEmpty(postInput.title)) {
+            errors.push({ message: 'Title is required'});
+        }
+        if (!validator.isLength(postInput.title, { min: 5 })) {
+            errors.push({ message: 'Title must be at least 5 characters long'});
+        }
+        if (validator.isEmpty(postInput.content)) {
+            errors.push({ message: 'Content is required'});
+        }
+        if (!validator.isLength(postInput.content, { min: 5 })) {
+            errors.push({ message: 'Content must be at least 5 characters long'});
+        }
+
+        if (errors.length > 0) {
+            const error = new Error('Validation Error');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+
+        post.title = postInput.title;
+        post.content = postInput.content;
+        
+        if (postInput.imageUrl !== 'undefined') {
+            post.imageUrl = postInput.imageUrl;
+        }
+
+        const updatedPost = await post.save();
+
+        return {
+            ...updatedPost._doc,
+            _id: updatedPost._id.toString(),
+            createdAt: updatedPost.createdAt.toISOString(),
+            updatedAt: updatedPost.updatedAt.toISOString()
         }
     }
 }
